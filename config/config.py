959a2ca3d9b1b7d04436f8b1c186cab38bf91bf7
Богдан <valera@>
2023-11-28 16:43:01 +0300

    ready

--- config/config.py
+++ config/config.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = '6885861535:AAEVB0ur3V1EOZJvhNHW-yL9Opo8wEBXZ-Q'
 
-SUPER_GROUP = os.environ.get('SUPER_GROUP')
+SUPER_GROUP = '-1002001968366'
